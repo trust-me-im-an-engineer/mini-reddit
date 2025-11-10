990e1677d5ddddc17b80d689061fef7fe5dcbde3
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-11-11 00:38:28 +0300

    format

--- migrations/001_init.up.sql
+++ migrations/001_init.up.sql
@@ -1,38 +1,42 @@
-CREATE TABLE IF NOT EXISTS posts (
-	id BIGINT PRIMARY KEY generated always AS identity,
-	author_id uuid NOT NULL,
-	title text NOT NULL,
-	content text NOT NULL,
-	created_at timestamptz NOT NULL DEFAULT NOW(),
-	rating int NOT NULL DEFAULT 0,
-	comments_restricted boolean NOT NULL DEFAULT false,
+CREATE TABLE IF NOT EXISTS posts
+(
+    id                  BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    author_id           uuid        NOT NULL,
+    title               TEXT        NOT NULL,
+    content             TEXT        NOT NULL,
+    created_at          timestamptz NOT NULL DEFAULT NOW(),
+    rating              INT         NOT NULL DEFAULT 0,
+    comments_restricted BOOLEAN     NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS comments (
-	id BIGINT PRIMARY KEY generated always AS identity,
-	post_id BIGINT NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
-	author_id uuid NOT NULL,
-	"text" text NOT NULL,
-	created_at timestamptz NOT NULL DEFAULT NOW(),
-	rating int NOT NULL DEFAULT 0,
-	deleted boolean NOT NULL DEFAULT false,
-	parent_id bigint REFERENCES comments(id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS comments
+(
+    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    post_id    BIGINT      NOT NULL REFERENCES posts (id) ON DELETE CASCADE,
+    author_id  uuid        NOT NULL,
+    "text"     TEXT        NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    rating     INT         NOT NULL DEFAULT 0,
+    deleted    BOOLEAN     NOT NULL DEFAULT FALSE,
+    parent_id  BIGINT REFERENCES comments (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS post_votes (
-	voter_id uuid NOT NULL,
-	post_id bigint NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
-	value smallint NOT NULL,
-	PRIMARY KEY (voter_id, post_id),
-	CHECK (value IN (1, -1)),
+CREATE TABLE IF NOT EXISTS post_votes
+(
+    voter_id uuid     NOT NULL,
+    post_id  BIGINT   NOT NULL REFERENCES posts (id) ON DELETE CASCADE,
+    value    SMALLINT NOT NULL,
+    PRIMARY KEY (voter_id, post_id),
+    CHECK (value IN (1, -1))
 );
 
-CREATE TABLE IF NOT EXISTS comment_votes (
-	voter_id uuid NOT NULL,
-	comment_id bigint NOT NULL REFERENCES comments(id) ON DELETE CASCADE,
-	value smallint NOT NULL,
-	PRIMARY KEY (voter_id, comment_id),
-	CHECK (value IN (1, -1)),
+CREATE TABLE IF NOT EXISTS comment_votes
+(
+    voter_id   uuid     NOT NULL,
+    comment_id BIGINT   NOT NULL REFERENCES comments (id) ON DELETE CASCADE,
+    value      SMALLINT NOT NULL,
+    PRIMARY KEY (voter_id, comment_id),
+    CHECK (value IN (1, -1))
 );
 
 CREATE INDEX posts_rating_id_idx ON posts (rating DESC, id ASC);
