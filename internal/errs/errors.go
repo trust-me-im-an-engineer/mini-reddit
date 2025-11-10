e656d49f5b94ef6d32d5370d112f33ac666f4143
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-11-11 02:19:54 +0300

    ParentCommentNotFound, CommentsRestricted

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -7,18 +7,22 @@ import (
 )
 
 var (
-	PostNotFound         = errors.New("post not found")
-	CommentNotFound      = errors.New("comment not found")
-	InvalidID            = errors.New("id must be valid integer")
-	CommentDeleted       = errors.New("comment is deleted")
-	ParentCommentDeleted = errors.New("cannot reply to deleted comment")
-	InvalidCursor        = errors.New("invalid cursor")
-	InternalServer       = errors.New("internal server error")
+	PostNotFound          = errors.New("post not found")
+	CommentNotFound       = errors.New("comment not found")
+	ParentCommentNotFound = errors.New("parent comment not found")
+	CommentsRestricted    = errors.New("comments disabled for this post")
+	InvalidID             = errors.New("id must be valid integer")
+	CommentDeleted        = errors.New("comment is deleted")
+	ParentCommentDeleted  = errors.New("cannot reply to deleted comment")
+	InvalidCursor         = errors.New("invalid cursor")
+	InternalServer        = errors.New("internal server error")
 )
 
 var all = []error{
 	PostNotFound,
 	CommentNotFound,
+	ParentCommentNotFound,
+	CommentsRestricted,
 	InvalidID,
 	CommentDeleted,
 	ParentCommentDeleted,
